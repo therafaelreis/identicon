a8dbdf8b8436b2533766f9f188958b7b2ed2f245
therafaelreis <therafaelreis@gmail.com>
2023-03-09 22:45:03 -0600

    :::added functionality to create draw and save the identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,7 +5,9 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
-    |> build_pixel_map()
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -50,14 +52,31 @@ defmodule Identicon do
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = Enum.map grid, fn({_code, index}) ->
-      horizontal = rem(index, 5) * 50
-      vertical = div(index, 5) * 50
+    pixel_map =
+      Enum.map(grid, fn {_code, index} ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
 
-      top_left = {horizontal, vertical}
-      bottom_right = {horizontal + 50, vertical + 50}
-      {top_left, bottom_right}
-    end
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+        {top_left, bottom_right}
+      end)
+
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
   end
 end
